92f0ad8cfe87da1e49aa70816e560a8d37dc87a6
Чусов Сергей <chusov.005@mail.ru>
2025-12-16 00:29:47 +0300

    Добавил проект Pets

---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Pet {
+public:
+    string name;
+    string type;
+    int age;
+
+    void save() {
+        ofstream file("pets.txt", ios::app);
+        file << name << " " << type << "" << age << endl;
+        file.close();
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    Pet pet;
+
+    cout << "Имя питомца: ";
+    cin >> pet.name;
+    cout << "Вид питомца: ";
+    cin >> pet.type;
+    cout << "Возраст: ";
+    cin >> pet.age;
+
+    pet.save();
+    cout << "Сохранено в файл" << endl;
+
+    return 0;
+}
